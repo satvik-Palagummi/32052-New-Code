0c3967a15ede5690e22632ba5d7b4115c94112ff
Satvik Palagummi <satvik.palagummi@gmail.com>
2025-12-07 01:22:54 -0600

    Constants code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auton/FirstAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auton/FirstAuton.java
@@ -1,4 +1,22 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.Auton;
 
-public class FirstAuton {
+import com.pedropathing.follower.Follower;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous
+public class FirstAuton extends OpMode {
+    private Follower follower;
+    private Timer pathTimer, opModeTimer;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
